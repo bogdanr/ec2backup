96fb9d2c8de560412237380faddd30d92eb9cf3e
Bogdan Rădulescu <bogdan@nimblex.net>
2013-12-23 13:26:53 +0000

    Migrated to AWS CLI tools from ec2-api-tools and eliminated credentials from config
    It is far more convenient to install aws cli (pip install awscli) and a lot better to configure profiles in a standard matter (~/.aws/config).
    With this approach the user that does backups can also access the tools manually in the console and perform checks or other tasks.
    For reference on how aws cli should be configured have a look here: https://github.com/aws/aws-cli

--- ec2backup.sh
+++ ec2backup.sh
@@ -45,13 +45,14 @@ fi
 . $CONF
 
 # Sanity checks
-command -v java >/dev/null 2>&1		|| { echo >&2 "Java was not detected. Make sure you can run java in the command line."; exit 1; }
-command -v ec2dsnap >/dev/null 2>&1	|| { echo >&2 "EC2 Tools were not detected. Make sure you can run ec2dsnap in the command line."; exit 1; }
+# command -v java >/dev/null 2>&1		|| { echo >&2 "Java was not detected. Make sure you can run java in the command line."; exit 1; }
+# command -v ec2dsnap >/dev/null 2>&1	|| { echo >&2 "EC2 Tools were not detected. Make sure you can run ec2dsnap in the command line."; exit 1; }
+command -v aws >/dev/null 2>&1		|| { echo >&2 "AWS CLI Tools were not detected. Make sure you can run aws in the command line."; exit 1; }
 
 
 # This function take the name of the volume as the single parameter
 create_snapshot() {
-  ec2addsnap -d "ec2backup-`date +%Y-%m-%d`" $1 | awk '{print "Snapshot " $2 " for volume " $3 " is " $4}'
+  aws --profile=$PROFILE ec2 create-snapshot --output=text --description="ec2backup-`date +%Y-%m-%d`" --volume-id $1 | awk '{print "Snapshot " $4 " for volume " $7 " is " $6}'
 }
 
 # This function takes two parameters, volume ID and days of retention
@@ -60,7 +61,7 @@ prune_snapshot() {
   day_days_ago_s=`date --date="$date_days_ago" +%s`
  
   # Get snapshot info for the Volume
-  ec2dsnap | awk "/$1/ && /ec2backup/ && /completed/" > /tmp/ec2_snapshots.txt 2>&1
+  aws --profile=$PROFILE ec2 describe-snapshots --output=text | awk "/$1/ && /ec2backup/ && /completed/" > /tmp/ec2_snapshots.txt 2>&1
  
   # Make sure we don't delete the last snapshot
   [[ `wc -l /tmp/ec2_snapshots.txt | cut -d " " -f1` -eq 1 ]] && Info skipping delete for the last snapshot of $1 && return
@@ -69,12 +70,12 @@ prune_snapshot() {
   while read line
   do
         snapshot_name=`echo "$line" | awk '{print $2}'`
-        snapshot_date=`echo "$line" | awk '{print $5}' | awk -F "T" '{printf "%s\n", $1}'`
+        snapshot_date=`echo "$line" | awk '{print $6}' | awk -F "T" '{printf "%s\n", $1}'`
         snapshot_date_s=`date --date="$snapshot_date" +%s`
  
         if (( $snapshot_date_s <= $day_days_ago_s )); then
                         Info "Deleting snapshot $snapshot_name for volume $1"
-                        ec2-delete-snapshot $snapshot_name
+                        aws --profile=$PROFILE ec2 delete-snapshot --snapshot-id $snapshot_name
 #        else
 #                        Info "NOT deleting snapshot $snapshot_name for volume $1"
         fi
@@ -82,7 +83,7 @@ prune_snapshot() {
 }
 
 # Get a list of volumes that are available in EC2
-AVAILABLE_VOLs=(`ec2dvol | awk '/VOLUME/ {print $2}'`)
+AVAILABLE_VOLs=(`aws --profile=$PROFILE ec2 describe-volumes --output=text | awk '/VOLUME/ {print $7}'`)
 
 # Here we only sanitize the volumes list keeping only volumes that are still available in EC2
 VBKP=($(comm -12 <(printf '%s\n' "${AVAILABLE_VOLs[@]}" | LC_ALL=C sort) <(printf '%s\n' "${VOLIDs[@]}" | LC_ALL=C sort)))
@@ -97,13 +98,14 @@ echo "for_backp " ${VOLIDs[@]}
 echo "intersect " ${VBKP[@]}
 echo "not ava   " ${RVOL[@]}
 echo ------------------------
-ec2dsnap | awk '/pending/'
+aws --profile=$PROFILE ec2 describe-snapshots --output=text | awk '/pending/'
 }
 
 main() {
   echo -e "________________________________________________\n`date`"
-  Warning Volumes ${RVOL[@]} are not available anymore
-
+  if [[ ${RVOL[@]} -gt 0 ]]; then
+    Warning Volumes ${RVOL[@]} are not available anymore
+  fi
   for volume in ${VBKP[@]}; do
     create_snapshot $volume
     prune_snapshot  $volume $RETAIN 
